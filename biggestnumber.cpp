035b7ce7e2851b6ce08b782055156bf649d46149
rpersons1 <rpersons1@mail.csuchico.edu>
2018-02-06 11:46:18 -0800

    has varibes for user input

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,11 +5,13 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
-  cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+int user1
+int user2
 
+  cout<<"Please enter a whole number:\n";
+  cin<<user1;
+  cout<<"Please enter another whole number:\n";
+  cin<<user2;
   cout<<"Of those two numbers, the biggest is: ";
   cout<<endl<<"Thank you for playing.\n";
 
