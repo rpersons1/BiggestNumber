fd7f64e630756de8b7ef8ffdf09ec3c87d340474
rpersons1 <rpersons1@mail.csuchico.edu>
2018-02-06 18:55:23 -0800

    made a if statment

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,15 +5,23 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-int user1
-int user2
+int user1;
+int user2;
+int biggest;
 
   cout<<"Please enter a whole number:\n";
-  cin<<user1;
+  cin>>user1;
   cout<<"Please enter another whole number:\n";
-  cin<<user2;
+  cin>>user2;
   cout<<"Of those two numbers, the biggest is: ";
-  cout<<endl<<"Thank you for playing.\n";
+  if(user1>=user2){
+  biggest=user1;
+  }
+  else
+  {
+  biggest=user2;
+  }
+  cout<<biggest<<endl<<"Thank you for playing.\n";
 
   return 0;
 }
